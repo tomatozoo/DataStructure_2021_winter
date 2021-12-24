b56c394c19b00e5b4ae0be5a333e89d419cd1c50
Young Zoo Park <enkeejuniour@naver.com>
2021-12-24 14:16:38 +0900

    Update day3.py
    update

--- day3/day3.py
+++ day3/day3.py
@@ -74,3 +74,114 @@ for num in [10,20,30,40]:
   
 
 print(fibo2(10))
+
+
+def htower(n, a, b):
+    global count
+    count += 1
+    if n==1:
+        print("Disc %d, moved from Pole (%d) to (%d)" % (n,a,b))
+    else:
+        c = 6-a-b # 1 + 2 + 3 = 6 # 비어있는 기둥
+        htower(n-1,a,c)
+        print("Disc %d, moved from Pole (%d) to (%d)" % (n,a,b))
+        htower(n-1, c, b)
+        
+"""count = 0
+n = int(input("Enter disc number:"))
+htower(n, 1, 2)
+print("# move for %d discs : %d" % (n, count))"""
+
+
+def permutation(word, i, n):
+    if i==n: print(word)
+    else:
+        for j in range(i, n+1):
+            
+            # L로 시작하는 그룹
+            # A로 시작하는 그룹
+            # N으로 시작하는 그룹
+            # G로 시작하는 그룹
+            
+            word[i], word[j] = word[j], word[i]
+            permutation(word, i+1, n)
+            word[i], word[j] = word[j], word[i]
+
+word = ['L', 'A', 'N', 'G']
+permutation(word, 0,3)
+
+class NQeens:
+    
+    def __init__(self, size): # 순열 문제와 같다. 
+        self.size = size
+        self.solutions = 0
+        self.col = [-1] * size
+        
+    def put_queen(self, row):
+        if row==self.size:
+            self.show_board()
+            self.solutions += 1
+        else:
+            for col in range(self.size):
+                if self.check_pos(row, col):
+                    self.col[row] = col
+                    # queen을 놓음
+                    self.put_queen(row+1)
+                    
+    def check_pos(self, rows, col):
+        # 잡아먹는 queen이 있는지 check
+        for i in range(rows):
+            if self.col[i] == col or \
+                self.col[i] - i == col - rows or \
+                self.col[i] + i ==col + rows:
+                    return False
+        return True
+            
+    def show_board(self):
+        for row in range(self.size):
+            line = ""
+            
+            for column in range(self.size):
+                if self.col[row] == column:
+                    line += 'Q'
+                else:
+                    line += "+ "
+            print(line)
+        print()
+
+N = 4
+q = NQeens(N)
+q.put_queen(0)
+print(q.solutions, "solutions found")
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def empty(self):
+        return len(self.stack)==0
+    def size(self):
+        return len(self.stack)
+    def push(self, item):
+        self.stack.append(item)
+        self.view()
+    def pop(self):
+        if not self.empty():
+            return self.stack.pop()
+        else:
+            print("Stack empty")
+    def view(self):
+        print(self.stack)
+    def peek(self):
+        if not self.empty():
+            return self.stack[self.size()-1]
+        else:
+            return None
+
+s = Stack()
+for item in [3,4,5,6,7,8]:
+    s.push(item)
+print("stack size", s.size())
+print("top", s.peek())
+
+for i in range(7):
+    print(s.pop(), end=' ')
