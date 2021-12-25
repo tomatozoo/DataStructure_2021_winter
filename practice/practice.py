b342c390f7807ef78bc7fb559a5c072650a6a925
Young Zoo Park <enkeejuniour@naver.com>
2021-12-25 13:10:52 +0900

    Create practice.py

---
+++ practice/practice.py
@@ -0,0 +1,92 @@
+# fibonacci
+
+
+# prime number
+
+
+# fraction
+
+    # __init__
+    
+    # __str__
+    
+    # __add__
+    
+    # multiply
+    
+    # gcd
+    
+    
+# bowling game
+
+# factorial
+
+# gcd
+
+# binary search
+
+# fibo1, fibo2
+
+# htower(n,a,b)
+
+# permutation
+
+# N Queens Problem
+
+    # __init__
+    
+    
+    # put_queen
+    
+    # check_pos
+    
+    # show_board
+    
+
+# stack
+
+    # __init__
+    
+    # empty
+    
+    # size
+    
+    # push
+    
+    # pop
+    
+    # view
+    
+    # peek
+    
+# queue
+
+    # __init__
+    
+    # empty
+    
+    # size
+    
+    # enqueue
+    
+    # dequeue
+    
+    # view
+    
+    # peek
+
+# CQueue
+
+    # __init__
+    
+    # empty
+    
+    # full
+    
+    # view
+    
+    # push
+    
+    # pop
+    
+    
