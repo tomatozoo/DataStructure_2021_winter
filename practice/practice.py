31c76b415b012449c67ca70e82a2d7c511bb027e
Young Zoo Park <enkeejuniour@naver.com>
2021-12-25 13:31:32 +0900

    Update practice.py

--- practice/practice.py
+++ practice/practice.py
@@ -1,21 +1,60 @@
 # fibonacci
-
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num-2)+fibonacci(num-1)
 
 # prime number
-
+def prime():
+    pass
 
 # fraction
-
-    # __init__
-    
+class fraction:
+    def __init__(self, num, dim):
+        self.num = num
+        self.dim = dim
+        
     # __str__
+    def __str__(self):
+        return '{0}/{1}'.format(self.num, self.dim)
     
     # __add__
+    def __add__(self, frac):
+        new = fraction(1,1)
+        new.num = self.num * frac.dim + self.dim * frac.num
+        new.dim = self.dim * frac.dim
+        common = self.gcd(new.num, new.dim)
+        new.num //= common
+        new.dim //= common
+        return new
     
     # multiply
+    def multiply(self, frac):
+        new = fraction(1,1)
+        new.num = self.num * frac.num
+        new.dim = self.dim * frac.dim
+        common = self.gcd(new.num, new.dim)
+        new.num //= common
+        new.dim //= common        
+        return new
     
     # gcd
-    
+    def gcd(self, a, b):
+        if b <= 1:
+            return a
+        else:
+            return self.gcd(b, a%b)
+
+f1 = fraction(1, 3)
+f2 = fraction(2, 4)
+
+print(f1)
+print(f2)
+print(f1+f2)  
+print(f1.multiply(f2))
     
 # bowling game
 
