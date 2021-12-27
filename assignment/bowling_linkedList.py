bf280432b8e6e667288be8927e136130e1a7805e
Young Zoo Park <enkeejuniour@naver.com>
2021-12-28 01:38:08 +0900

    Update bowling_linkedList.py

--- assignment/bowling_linkedList.py
+++ assignment/bowling_linkedList.py
@@ -1,47 +1,38 @@
 class frame:
-    def __init__(self, index):
+    def __init__(self, index, first, second, result):
         self.index = index
-        self.first = 0
-        self.second = 0
+        self.first = first
+        self.second = second
+        self.result = result
         self.next = None
-        self.result = None
-        self.f_total = 0
         
-    def getNumber(self):
-        if self.index < 10:
-            self.first, self.second = input(f"{self.index+1} 프레임 : ").split(' ')
-        else:
-            self.first, self.second = input("보너스 드로우 : ").split(' ')
-        
-        self.f_total = int(self.first) + int(self.second)
-        
-    def current(self):
-        if self.index < 10:
-            if int(self.first) == 10:
-                self.result = 'X'
-            elif int(self.first) + int(self.second) == 10:
-                self.result = '/'
-            else:
-                self.result = '-'
-                
-            
 class bowling:
     def __init__(self):
         self.head = None
         self.count = None
-    def calculate(self):
-        pass
-    def pastCal(self):
-        pass
 
-def bowling():
+def bowl():
     
     total = 0
-    result = 'X'
-
+    b = bowling()
+    
     for i in range(11):
-        pass
+        if i < 10:
+            first, second = input(f"{i+1} 프레임 : ").split(' ')
+        else:
+            first, second = input("보너스 드로우 : ").split(' ')
+        f_total = int(first) + int(second)
+
+        if i < 10:
+            if int(first) == 10:
+                # strike
+                result = 'X'
+                tmp = frame(i, int(first), int(second), result, ' ')
+            elif int(first) + int(second) == 10:
+                pass
+                
 
     
     
-bowling()
+    
+bowl()
