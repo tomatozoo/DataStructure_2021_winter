50ed7bbb0573ae27c8c42a1dae769f17a027f4bb
Young Zoo Park <enkeejuniour@naver.com>
2021-12-28 14:23:15 +0900

    Update sll.py

--- day5/sll.py
+++ day5/sll.py
@@ -58,3 +58,75 @@ def pop_rear(self):
             self.head = None
         del temp
         return item
+
+def poly_add(a,b,d):
+    pass
+def del_list(self):
+    node = self.head
+    while node:
+        temp = node
+        node = node.link
+        del temp
+    print("List is deleted")
+    self.head = None
+    
+def concat(self, second):
+    if not self.head:
+        return second
+    elif second:
+        temp = self.head
+        while temp.link:
+            temp = temp.link 
+        temp.link = second
+    return self.head
+# Time Complexity = O(n) (n= length of first)
+
+def length_list(self):
+    if not self.head: return 0
+    count = 0
+    temp = self.head
+    while True:
+        count += 1
+        temp = temp.link
+        if temp == self.head: break
+    return count
+
+def add_sort(self, item):
+    node = Node(item)
+    if not self.head:
+        self.head = node
+        self.tail = node
+        node.link = self.head
+        return
+    temp = self.head
+    prev = None
+    while temp:
+        if item > temp.data:
+            prev = temp
+            temp = temp.link
+        elif item == temp.data:
+            print("Duplicate item")
+            return
+        else:
+            node.link = temp
+            if prev:
+                prev.link = node
+            else:
+                self.head = node
+                self.tail.link = node
+            return
+        if temp == self.head: break
+    node.link = self.head
+    prev.link = node
+    
+def reverse(self):
+    first = self.head
+    second = third = None
+    self.tail = self.head
+    while first:
+        third = second
+        second = first
+        first = first.link
+        second.link = third
+    self.head = second
+    return 
