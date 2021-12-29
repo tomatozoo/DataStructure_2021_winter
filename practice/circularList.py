eca740e965e1eee7522d8207e4a5fcd2dd2deb44
Young Zoo Park <enkeejuniour@naver.com>
2021-12-29 22:03:11 +0900

    Create circularList.py

---
+++ practice/circularList.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, item):
+        self.data = item
+        self.link = None    
+
+class CL:
+    def __init__(self):
+        pass
+    def length_list(self):
+        pass
+    def add_sort(self, item):
+        pass
+
