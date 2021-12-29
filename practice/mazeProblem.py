61577624b138798aeafc59f260c1f57ffe964279
Young Zoo Park <enkeejuniour@naver.com>
2021-12-29 20:40:05 +0900

    .

--- practice/mazeProblem.py
+++ practice/mazeProblem.py
@@ -8,7 +8,7 @@ class Maze:
                      [1,0,0,1,0,1,1,0,1,1],\
                      [1,1,0,0,1,0,0,0,1,1],\
                      [1,1,1,0,0,0,1,0,0,1], \
-                     [1,0,0,0,1,0,1,1,1,1],\
+                     [1,0,0,0,1,0,1,1,0,1],\
                      [1,1,1,1,1,1,1,1,1,1]]
         self.mark = [[0]*10 for i in range(10)]
         self.stack = [[1,1]]
@@ -22,32 +22,40 @@ class Maze:
             print(i)
     def explore(self):
         while True:
+            self.mark[1][1] = 1
             if len(self.stack) == 0:
                 print("FAILED")
                 return 
-            elif self.stack[-1] == [9,9]:
+            elif self.stack[0] == [8,8]:
                 print("Solution : ", self.stack)
                 return
             else:
                 self.found = False
                 tmp = self.stack[0]
-                if self.maze[tmp[0]-1][tmp[1]]==0:
+                if self.maze[tmp[0]-1][tmp[1]]==0 and \
+                    self.mark[tmp[0]-1][tmp[1]]==0 :
                     self.found = True
                     self.nextCandid = [tmp[0]-1, tmp[1]]
-                elif self.maze[tmp[0]+1][tmp[1]]==0:
+                elif self.maze[tmp[0]+1][tmp[1]]==0 and \
+                    self.mark[tmp[0]+1][tmp[1]]==0 :
                     self.found = True     
                     self.nextCandid = [tmp[0]+1, tmp[1]]           
-                elif self.maze[tmp[0]][tmp[1]-1]==0:
+                elif self.maze[tmp[0]][tmp[1]-1]==0 and \
+                    self.mark[tmp[0]][tmp[1]-1]==0 :
                     self.found = True          
                     self.nextCandid = [tmp[0], tmp[1]-1]       
-                elif self.maze[tmp[0]][tmp[1]+1]==0:
+                elif self.maze[tmp[0]][tmp[1]+1]==0 and \
+                    self.mark[tmp[0]][tmp[1]+1]==0 :
                     self.found = True 
                     self.nextCandid = [tmp[0], tmp[1]+1]
+                    
                 if self.found == True:
                     self.stack.insert(0, self.nextCandid)
+                    self.mark[self.nextCandid[0]][self.nextCandid[1]] = 1
                     print(self.stack)
                 else:
-                    self.stack.pop()
+                    tmp = self.stack.pop(0)
+                    
 
 
 
---
+++ practice/singlyLinkedlist.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self):
+        pass
+
+class SLL:
+    def __init__(self):
+        pass
+    def isEmpty(self):
+        pass
+    def addFront(self, node):
+        pass
+    def addRear(self, node):
+        pass
+    def addSort(self, node):
+        pass
+    def delFind(self, node):
+        pass
+    def popFront(self, node):
+        pass
+    def popRear(self, node):
+        pass
+    def delAll(self):
+        pass
+    def view(self):
+        pass
+    def length(self):
+        pass
+    def reverse(self):
+        pass
+    def concat(self):
+        pass
+    def peekFront(self):
+        pass
+    def peekRear(self):
+        pass
