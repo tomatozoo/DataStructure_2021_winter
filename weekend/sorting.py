7a9966be768075e4b5e02543e1e452e6725919b4
Young Zoo Park <enkeejuniour@naver.com>
2022-01-08 22:43:45 +0900

    .

--- weekend/sorting.py
+++ weekend/sorting.py
@@ -72,7 +72,6 @@ class InsertionSort:
                 else:
                     continue
             
-
 num = [31,9,10,23,49,15,11,7]
 s = InsertionSort(num)
 s.sort()
@@ -85,10 +84,78 @@ class shellSort:
         for i in self.lst:
             print("%2d " % i)
     def sort(self):
-        pass
+        gap = len(self.lst) // 2
+        while True:
+            if gap < 1:
+                break
+            for i in range(0, len(self.lst),gap):
+                tmp = self.lst[i]
+                for j in range(0, i, gap):
+                    if tmp<= self.lst[j]:
+                        t = self.lst[i]
+                        self.lst[i] = self.lst[j]
+                        self.lst[j] = t
+            gap //= 2
+            print(gap)
+            
+            
             
 
 num = [31,9,10,23,49,15,11,7]
 s = shellSort(num)
 s.sort()
+print(s.lst)
+
+class QuickSort:
+    def __init__(self, num):
+        self.lst = num
+        
+    def swap(self, a, b):
+        tmp = self.lst[a]
+        self.lst[a] = self.lst[b]
+        self.lst[b] = tmp
+    def MedianofThree(self,  left, right):
+        mid = self.lst[(left+right)//2]
+        lft = self.lst[left]
+        rt = self.lst[right]
+        if (mid <= rt <= lft) or (lft <= rt <= mid):
+            return right
+        elif  (rt < mid < lft) or (lft <= mid <= rt):
+            return (left+right)//2
+        else:
+            return left
+    def sort(self, left, right):
+        print(self.lst)
+        # 재귀적인 반복
+        if left == right:
+            return
+        pivot = self.MedianofThree(left, right)
+        lft = left
+        rt = right
+        # partitining
+        while True:
+            if lft >= rt:
+                self.swap(pivot, rt)
+                break
+            if self.lst[lft] > pivot:
+                pass
+            else:
+                lft += 1
+            if self.lst[rt] < pivot:
+                pass
+            else:
+                rt +- 1  
+            if  self.lst[lft] > pivot and self.lst[rt] < pivot:
+                self.swap(lft, rt)
+        self.sort(self, left, pivot)
+        self.sort(self, pivot, right)
+
+            
+print("HI")
+
+num = [31,9,10,23,49,15,11,7]
+s = QuickSort(num)
+print(s.lst)
+s.sort(0, len(num)-1)
+print(s.lst)
 print(s.lst)
