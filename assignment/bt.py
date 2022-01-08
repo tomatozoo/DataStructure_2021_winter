a50cb050f225faffce7a20d65aa64e909fddb6f9
Young Zoo Park <enkeejuniour@naver.com>
2022-01-08 13:34:42 +0900

    .

--- assignment/bt.py
+++ assignment/bt.py
@@ -7,6 +7,8 @@ class Node:
 class BinaryTree:
     def __init__(self):
         self.root = None
+        self.treeDepth = 0
+        self.treeHeight = 0
         
         temp = Node('-')
         self.root = temp
@@ -45,15 +47,17 @@ class BinaryTree:
             self.printBT(tree.left)
             self.printBT(tree.right)
     
-    
-    def depth_node(self, node, d):
+    # 탐색
+    def depth_node(self, node, item):
         if node:
             try:
-                print("%d %d" % (node.data, d))
+                print("%d %d" % (node.data, item))
             except:
-                print("%s %d" % (node.data, d))
-            self.nodeDepth(node.left, d+1)
-            self.nodeDepth(node.right, d+1)   
+                print("%s %d" % (node.data, item))
+            self.depth_node(node.left, item+1)
+            self.depth_node(node.right, item+1)   
+        else:
+            return -1
              
     def nodeCalculate(self, node):
         if node.left or node.right:
@@ -61,36 +65,54 @@ class BinaryTree:
         else:
             return 0
     
-    def height_node(self, node):
+    # 탐색
+    def height_node(self, node, item):
         if node:
             try:
                 print("%d" % (node.data), end=' ')
             except:
                 print("%s" % (node.data), end =' ')   
             print("%d" % self.nodeCalculate(node)) 
-            self.nodeHeight(node.left)
-            self.nodeHeight(node.right) 
+            self.height_node(node.left, item)
+            self.height_node(node.right, item) 
         else:
             return 0  
-            
-    def treeDepth(self, root, max):
-        if root:
-            self.height_node(root.left)
-            try:
-                print("%d" % (root.data), end=' ')
-            except:
-                print("%s" % (root.data), end =' ')  
-            print("%d" % self.nodeCalculate(root))
-            self.height_node(root.right)
-        else:
-            return 0
+
+    def depth(self, root):
+        self.finddepth(root, 0)
+        print(f'tree depth : {self.treeDepth}')
         
-    def treeHeight(self):
-        pass
+    def finddepth(self, root, depth):
+        # return 조건
+        if not root:
+            return 0
+        else:
+            if self.treeDepth < depth:
+                self.treeDepth = depth
+            self.finddepth(root.left, depth+1)
+            self.finddepth(root.right, depth+1)
+        
+    def height(self, root):
+        self.findheight(self.root, 0) # 결국 root의 depth를 찾는다
+        print(f'tree height : {self.treeHeight}')
+        return 0 
+    
+    def findheight(self, root, height):
+        if root:
+            if self.treeHeight < height:
+                self.treeHeight = height
+            self.findheight(root.left, height+1)
+            self.findheight(root.right, height+1) 
+        else:
+            return 0  
             
 c = BinaryTree()
 c.printBT(c.root)
 print('DDDDDDD')
 c.depth_node(c.root, 0)
 print('HHHHHH')
-c.height_node(c.root)
+c.height_node(c.root, 0)
+print("TDTDTDTD")
+c.depth(c.root)
+print("HHHHDDDD")
+c.height(c.root)
--- assignment/huffman.py
+++ assignment/huffman.py
@@ -1,26 +1,97 @@
+import copy
 class Node:
-    pass
+    def __init__(self, char, num):
+        self.right = None
+        self.left = None
+        self.data = char
+        self.freq = num
+        self.huff = []
+        
 class Huffman:
     def __init__(self, char, num):
+        self.root = None
         self.char = [char for _, char in sorted(zip(num, char))]
         self.num = [num for num, _ in sorted(zip(num, char))]
-        print(self.char)
+        self.nodes = []
+    
+    def selectTwo(self, num):
+        minVal = num[0] + num[1]
+        minFir = 0
+        minSec = 1
+        
+        for i in range(len(num)-1):
+            if num[i] + num[i+1] < minVal:
+                minVal = num[i] + num[i+1]
+                minFir = i
+                minSec = i+1
+        
+        return minFir, minSec
+    
+    def createTree(self):
+        print("Huffman coding tree")
+        
+        # step 0. sort char and num
         print(self.num)
-    def tree(self):
-        pass
+        
+        # step 1. create terminal node
+        for i in range(len(self.char)):
+            tmp = Node(self.char[i], self.num[i])
+            self.nodes.append(tmp)
+         
+        # step 2. create parent node
+        while True:
+            try:
+                if len(self.num) <= 1:
+                    break
+                
+                # 인접한 두 개를 더하는 것 중에 제일 작은 노드를 찾는다. 
+                first, second = self.selectTwo(self.num)
+                # 단말 노드를 합해 부모 노드를 만든다
+                tmp = Node('', self.nodes[first].freq+self.nodes[second].freq)
+                tmp.left = self.nodes[first]
+                tmp.right = self.nodes[second]
+                 
+                # node 리스트, num 리스트를 재조정해준다
+                self.nodes.pop(first)
+                self.nodes.pop(second-1)
+                self.num.pop(first)
+                self.num.pop(second-1)
+                self.nodes.insert(first, tmp)
+                self.num.insert(first, tmp.freq)
+                
+                # 현 상태 출력
+                print(self.num)
+            except:
+                pass
+        self.root = tmp
+        
+    def createCode(self, root, code):
+        if root.left == None and root.right == None:
+            root.huff = code
+            print(root.data, root.huff)
+            return
+        else:
+            root.huff = code
+            left = copy.deepcopy(code)
+            left.append('0')
+            right = copy.deepcopy(code)
+            right.append('1')
+            self.createCode(root.left, left)
+            self.createCode(root.right, right)
+        # root 노드에서 시작한다.   
+        # 탐색하면서 왼족에는 0, 오른족에는 1을 append 해준다.     
+          
     def codes(self):
-        pass        
+        print("Huffman codes")      
+        self.createCode(self.root, [])  
 
 # character
-char = ['w', 'f', 'r', 'm', 'e', 'd', 'o', 't', 'i', 'a', 'n',' ']
+char = ['w', 'f', 'r', 'm', 'e', 'd', 'o', 't', 'i', 'a', 'n','\' \'']
 # frequency
-num = [3,3,2,2,6,4,4,3,1,1,2,1]
+num =  [3, 3, 2, 2, 6, 3, 3, 2, 1, 1, 2, 1]
 
-print(len(char), len(num))
-
-# step 0. sort char and num
-char = [char for _, char in sorted(zip(num, char))]
-num = [num for num, _ in sorted(zip(num, char))]
-print(char)
-print(num)
-
+h = Huffman(char, num)
+print()
+h.createTree()
+print()
+h.codes()
