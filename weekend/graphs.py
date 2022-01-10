e1f940c391b7841da7ea8b385c3e5adc9293651f
Young Zoo Park <enkeejuniour@naver.com>
2022-01-10 21:47:22 +0900

    Update graphs.py

--- weekend/graphs.py
+++ weekend/graphs.py
@@ -2,6 +2,8 @@ class graph:
     def __init__(self, g):
         self.graph = g
         self.label = []
+        self.visit = [] # queue
+        self.l = []
     def dfs(self, vertex):
         print(vertex)
         self.label.append(vertex)
@@ -12,36 +14,23 @@ class graph:
             if j == vertex:
                 if i not in self.label:
                     self.dfs(i)
-    def bfs(self):
-        pass
+    def bfs(self, vertex):
+        self.visit.append(vertex)
+        self.l.append(vertex)
+        while len(self.visit) > 0:
+            tmp = self.visit.pop(0)
+            print(tmp)
+            for i, j in self.graph:
+                if i == tmp and j not in self.l:
+                    self.visit.append(j)
+                    self.l.append(j)
+                if j == tmp and i not in self.l:
+                    self.visit.append(i)
+                    self.l.append(i)
+            
+            
 # undirected graph
 gr = [(1,2), (1,4), (2,5), (2,3), (3,5), (6,9), (8,9), (10,11), (7,7)]
 gr = [(1,2),(1,4), (2,5), (4,5), (3,5)]
 g = graph(gr)
-g.dfs(1)
-g.bfs()
-
-class Node:
-    def __init__(self, value):
-        self.data = value
-class Graph:
-    def __init__(self):
-        self.graph = {}
-        self.visit = []
-    def create(self, v, data):
-        node = Node(data)
-        if v not in self.graph:
-            self.graph[v] = []
-        self.graph[v].append(node)
-    def dfs(self, v):
-        self.visit.append(v)
-        for node in self.graph[v]:
-            if node.data not in self.visit:
-                self.dfs(node.data)
-g = Graph()
-for v, node in [(1,2), (1,3), (2,1), (2,4), (2,5), (2,7), (3,1), (3,4), \
-(3,5), (4,2), (4,3), (4,7), (5,2), (5,3), (5,6), (5,8), \
-(6,5), (6,7), (6,8), (7,2), (7,4), (7,6), (8,5), (8,6)]:
-    g.create(v, node)
-g.dfs(1)
-print("dfs ", g.visit)
+g.bfs(1)
